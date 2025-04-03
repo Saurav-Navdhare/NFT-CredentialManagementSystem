8c987287e9641cc421e5a38de961c5d7e50c05e9
Saurav Navdhare <navdharesaurav2003@gmail.com>
2025-04-03 21:22:18 +0530

    fix(client): remove client: merging new git repo

--- client/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import thirdWebReducer from "../helpers/thirdWeb/thirdWebSlice";
-
-
-const appStore = configureStore({
-    reducer: thirdWebReducer
-});
-
-export default appStore;
